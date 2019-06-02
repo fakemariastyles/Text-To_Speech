4364ab5896988094d2ae018928431ea05249c7dc
mary <fakemariastyles>
2019-06-02 20:25:25 +0430

    layout editted , testings deleted

--- app/src/main/java/com/mmb/texttospeech/app/MainActivity.kt
+++ app/src/main/java/com/mmb/texttospeech/app/MainActivity.kt
@@ -2,6 +2,7 @@ package com.mmb.texttospeech.app
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.mmb.texttospeech.R
 import com.mmb.texttospeech.data.repository.SpeechRepository
@@ -11,14 +12,22 @@ class MainActivity : AppCompatActivity() {
 
     @Inject
     lateinit var speechRepository: SpeechRepository
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_text_to_speech_layout)
         TextToSpeechApp.component.inject(this)
-        speechRepository.textToSpeech("en-us", "Hello")
+
+
+        val editText = findViewById<EditText>(R.id.text)
+//        speechRepository.textToSpeech("en-us", "Hello Maryam")
         findViewById<Button>(R.id.play).setOnClickListener {
-            speechRepository.play(this)
+            editText.text.toString().let {
+                println(it)
+                speechRepository.textToSpeech("en-us", text = it)
+                speechRepository.play(this)
+            }
         }
+
+
     }
 }
--- app/src/main/java/com/mmb/texttospeech/data/repository/SpeechRepository.kt
+++ app/src/main/java/com/mmb/texttospeech/data/repository/SpeechRepository.kt
@@ -2,7 +2,6 @@ package com.mmb.texttospeech.data.repository
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.provider.MediaStore
 import android.util.Log
 import com.mmb.texttospeech.local.datasource.SpeechLocalDataSource
 import com.mmb.texttospeech.mediaplayer.AudioPlayer
@@ -10,8 +9,7 @@ import com.mmb.texttospeech.remote.datasource.SpeechRemoteDataSource
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-import java.io.*
-import java.util.*
+import java.io.InputStream
 import javax.inject.Inject
 
 class SpeechRepository @Inject constructor(
@@ -21,15 +19,13 @@ class SpeechRepository @Inject constructor(
 
     @SuppressLint("CheckResult")
     fun textToSpeech(language: String?, text: String?) {
-        Log.v("SS" , "requesting")
+        Log.v("SS", "requesting")
         speechRemoteDataSource.TextToSpeech(language, text)
             .subscribeOn(Schedulers.io())
             .observeOn(Schedulers.io())
             .subscribe({
                 println(it)
-                saveSpeech(
-                    Single.just(it)
-                )
+                saveSpeech(Single.just(it))
             }, {
                 println(it)
             })
@@ -37,17 +33,21 @@ class SpeechRepository @Inject constructor(
 
     @SuppressLint("CheckResult")
     private fun saveSpeech(inputStream: Single<InputStream>) {
-        Log.v("SS" , "onSaveSpeechRepo")
+        Log.v("SS", "onSaveSpeechRepo")
         inputStream.subscribeOn(Schedulers.io())
             .doOnSuccess {
                 speechLocalDataSource.saveFile(it)
             }
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({}, {})
+            .subscribe({
+            }, {
+            })
+
+
     }
 
 
-    fun play(context: Context){
-        AudioPlayer(context = context ,file =  context.openFileInput("speech.mp3")).play()
+    fun play(context: Context) {
+        AudioPlayer(context = context, file = context.openFileInput("speech.mp3")).play()
     }
 }
