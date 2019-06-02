4d5e1b8f881b2126da57dcf0425df1eb87fb5218
mary <fakemariastyles>
2019-06-02 21:10:05 +0430

    updated theme

--- app/src/main/java/com/mmb/texttospeech/app/MainActivity.kt
+++ app/src/main/java/com/mmb/texttospeech/app/MainActivity.kt
@@ -1,19 +1,19 @@
 package com.mmb.texttospeech.app
 
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.mmb.texttospeech.R
 import com.mmb.texttospeech.data.repository.SpeechRepository
-import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
     @Inject
     lateinit var speechRepository: SpeechRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_text_to_speech_layout)
@@ -24,16 +24,14 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        findViewById<Button>(R.id.play).setOnClickListener {
+        findViewById<View>(R.id.play).setOnClickListener {
             editText.text.toString().let {
-                if(speechRepository.isConnectedToInternet()){
+                if (speechRepository.isConnectedToInternet()) {
                     speechRepository.textToSpeech("en-us", text = it)
-                }
-                else{
-                    Toast.makeText(this, "Connect to internet first" , Toast.LENGTH_LONG)
+                } else {
+                    Toast.makeText(this, "Connect to internet first", Toast.LENGTH_LONG)
                         .show()
                 }
-
             }
         }
 
